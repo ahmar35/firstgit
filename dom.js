b01ca0a4d4047fba6be30c38f5fda03cf88cea4a
ahmar35 <muhammadahmar35@gmail.com>
2022-11-08 09:11:46 +0530

    again

--- dom.js
+++ dom.js
@@ -1,19 +1,9 @@
-var li=document.getElementsByTagName('li');
-console.log(li)
-console.log(li[3])
-console.log(li[4])
-
-
-li[4].textContent='Newitem';
-
-
-
-
-
-item=document.getElementsByClassName('list-group-power');
-console.log(item[4])
-console.log(item)
-
-item[0].textContent='Neitem';
-
-
+var items =document.querySelectorAll("li");
+console.log(items[1])
+items[1].style.backgroundColor="green"
+var odd=document.querySelectorAll("li:nth-Child(odd)")
+for (let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor="green"
+}
+var invisible=document.querySelectorAll('li')
+invisible[2].style.visibility="hidden"

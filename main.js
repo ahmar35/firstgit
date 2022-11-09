a841386103a0ec3a18c2ae4a819a15f073646a5c
ahmar35 <muhammadahmar35@gmail.com>
2022-11-09 11:43:00 +0530

    agaim123

--- main.js
+++ main.js
@@ -8,11 +8,14 @@ filter.addEventListener("keyup",filteritems)
 function addItem(e){
     e.preventDefault()
 
-
+var description=document.getElementById("description").value
 var newitem=document.getElementById("item").value;
 var li=document.createElement('li')
 li.className="list-group-item"
+const descriptionNode=document.createTextNode(description)
 li.appendChild(document.createTextNode(newitem));
+
+li.appendChild(descriptionNode)
 var deleteBtn=document.createElement("button")
 deleteBtn.className="btn btn-danger btn-sm float-right delete"
 deleteBtn.appendChild(document.createTextNode("X"))
@@ -34,7 +37,8 @@ function filteritems(e){
     var items=itemList.getElementsByTagName("li")
     Array.from(items).forEach(function(item){
         var itemname=item.firstChild.textContent
-        if(itemname.toLowerCase().indexOf(text)!=-1){
+        var description=item.childNodes[1].textContent
+        if(itemname.toLowerCase().indexOf(text)!=-1 || description.toLowerCase().indexOf(description)!=-1) {
             item.style.display="block"
         } else{
             item.style.display="none"

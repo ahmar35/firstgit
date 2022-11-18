d4ba1f753bc03f0238f8151af386fa03448dcd11
ahmar35 <muhammadahmar35@gmail.com>
2022-11-18 23:20:15 +0530

    new

--- tesr.js
+++ tesr.js
@@ -1,6 +1,10 @@
-console.log(pn());
-console.log(a);
-var a=3
-var pn =(nam) =>{
-    console.log(nam)
+var obj={num:2}
+var addToThis=function(a,b,c){
+    return this.num+a+b+c;
 }
+console.log(addToThis.call(obj,1,2,3))
+
+var arr=[1,2,3];
+console.log(addToThis.apply(obj,arr))
+var bound=addToThis.bind(obj)
+console.log(bound(1,2,3))

a8122925d3da85df6589b89be8548c23a6902c95
ahmar35 <117497933+ahmar35@users.noreply.github.com>
2022-12-24 13:09:43 +0530

    Delete tesr.js

--- tesr.js
+++
@@ -1,6 +0,0 @@
-function edituser(expense,description,category){
-    document.getElementById('expenseamount').value=expenseAmount
-    document.getElementById('choosedescription').value=description
-    document.getElementById('choosecategory').value=category
-    deleteuser(description)
-}

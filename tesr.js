09ac4bf4d0f494834f909e5d4825afad7bfb2d71
ahmar35 <muhammadahmar35@gmail.com>
2022-12-24 12:40:08 +0530

    expense tracker

--- tesr.js
+++ tesr.js
@@ -1,10 +1,6 @@
-var obj={num:2}
-var addToThis=function(a,b,c){
-    return this.num+a+b+c;
+function edituser(expense,description,category){
+    document.getElementById('expenseamount').value=expenseAmount
+    document.getElementById('choosedescription').value=description
+    document.getElementById('choosecategory').value=category
+    deleteuser(description)
 }
-console.log(addToThis.call(obj,1,2,3))
-
-var arr=[1,2,3];
-console.log(addToThis.apply(obj,arr))
-var bound=addToThis.bind(obj)
-console.log(bound(1,2,3))
